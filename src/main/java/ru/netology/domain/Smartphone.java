76f4bf5ca1cd23b0e629ff92157804defc0c9e69
Alexey Kalyakin <paxclub@yandex.ru>
2022-03-18 14:10:56 +0300

    added code

---
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -0,0 +1,22 @@
+package ru.netology.domain;
+
+public class Smartphone extends Product {
+    private String manufacturer;
+
+    public Smartphone() {
+        super();
+    }
+
+    public Smartphone(int id, String name, int price, String manufacturer) {
+        super(id, name, price);
+        this.manufacturer = manufacturer;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+}
---
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -0,0 +1,48 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Product;
+import ru.netology.repository.ProductRepository;
+
+public class ProductManager {
+    private ProductRepository repository;
+
+    public ProductManager(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public void saveProduct(Product product) {
+        repository.save(product);
+    }
+
+    public Product[] searchBy(String text) {
+        Product[] result = new Product[0];
+        for (Product product : repository.findAll()) {
+            if (matches(product, text)) {
+                Product[] tmp = new Product[result.length + 1];
+                for (int i = 0; i < result.length; i++) {
+                    tmp[i] = result[i];
+                }
+                tmp[tmp.length - 1] = product;
+                result = tmp;
+            }
+        }
+        return result;
+    }
+
+    public boolean matches(Product product, String search) {
+        if (product.getName().contains(search)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Product[] findAllProducts() {
+        return repository.findAll();
+    }
+
+    public Product[] removeByIdProducts(int id) {
+        return repository.removeById(id);
+    }
+
+}
